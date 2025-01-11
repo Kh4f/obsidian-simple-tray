a4bcb6666106e1e6573a5da70beb2f2bef7f64dc
Kh4f <khafg050@gmail.com>
2025-01-11 05:25:19 +0300

    fix: taskbar icon doesn't open minimized Obsidian

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const obsidian = require("obsidian");
-const {Tray, Menu, getCurrentWindow, BrowserWindow} = require("electron").remote;
+const {app, Tray, Menu, getCurrentWindow, BrowserWindow} = require("electron").remote;
 const path = require("path");
 
 
 const win = getCurrentWindow();
+
+app.on("second-instance", (e) => {
+	const win2 = BrowserWindow.getAllWindows()[0];
+
+	win2.setOpacity(0.0); // prevents "flashing" (quick open->close Obsidian start window)
+	win2.once("ready-to-show", () => {
+		win2.close();
+	});
+
+	win.show();
+	win.focus();
+});
 
 module.exports = class TrayPlugin extends obsidian.Plugin {
 	async onload() {
